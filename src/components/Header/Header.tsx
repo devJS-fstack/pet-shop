f477932e634fd43b68d38357de59ef8bc66d236f
Tinh Nguyen <tinh.nguyen@pascal.studio>
2023-02-12 13:19:24 +0700

    build header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,55 @@
-import { SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd"
+import Link from "next/link";
+
+const navs = 
+[
+    {
+        href: "cho-canh",
+        name: "Chó cảnh"
+    },
+    {
+        href: "meo-canh",
+        name: "Mèo Cảnh"
+    },
+    {
+        href: "cac-canh",
+        name: "Cặc Cảnh"
+    },
+    {
+        href: "lon-canh",
+        name: "Lồn cảnh"
+    },
+]
 
 const Header = () => (
     <div className="header-wrapper">
-        <div className="header-main hide-for-sticky">
+        <div className="header-main">
             <div className="header-inner flex-row container logo-left">
-                <div className="flex-col logo">
-                    <span>
-                        <img width="200" height="100" src="https://dogily.vn/wp-content/uploads/2020/07/dogily-logo.png" 
+                <div id="logo" className="flex-col logo">
+                    <Link href="">
+                        <img width="200" height="83" src="https://dogily.vn/wp-content/uploads/2020/07/dogily-logo.png" 
                         className="header_logo header-logo entered lazyloaded"/>
-                    </span>
+                    </Link>
                 </div>
-                <div className="flex-col hide-for-medium flex-left flex-grow">
+                <div className="flex-col flex-left flex-grow">
                     <form className="search-form">
-                        <div className="flex-row relative">
-                            <div className="flex-col flex-grow">
-                                <label className="screen-reader-text">Tìm kiếm:</label>
-                                <input type="search" className="search-field mb-0" placeholder="Tìm kiếm…" value="" />
-                            </div>
-                            <div className="flex-col">
-                                <SearchOutlined/>
-                            </div>
-                        </div>
+                        <Input.Search placeholder="Tìm kiếm..." size="large" allowClear/>
                     </form>
+                </div>
+            </div>
+        </div>
+        <div className="header-bottom">
+            <div className="flex-row container">
+                <div className="flex-col hide-for-medium flex-center">
+                    <ul className="nav header-nav header-bottom-nav nav-center">
+                        {
+                            navs.map(nav => (
+                                <li>
+                                    <Link href={nav.href} key={nav.href} className="nav-top-link">{nav.name}</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
